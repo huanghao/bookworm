9e1a7d6ec0ef7281207755e8bfed91d2ffe4fc7a
huanghao <huang1hao@gmail.com>
2013-06-18 07:56:26 +0800

    Change usage of index.py(have't completed)

--- index.py
+++ index.py
@@ -72,22 +72,63 @@ class DB(object):
         self.db.replace_document(idterm, doc)
 
 
+def Indexer(object):
+    pass
+
+
+def main(args):
+    if not os.path.isdir(args.db_path):
+        logger.error('db path does not exist(%s), '
+        'please use -d to specify a correct db path' % args.db_path)
+        return 1
+    if not os.path.isdir(args.repo_path):
+        logger.error('repo path does not exist(%s), '
+        'please use -r to specify a correct repo path' % args.repo_path)
+        return 1
+
+    indexer = Indexer(args.db_path, args.repo_path)
+    if args.pdf:
+        indexer.index(args.pdf)
+        return 0
+
+    input_ = None
+    if args.pdf_list_file:
+        input_ = open(args.pdf_list_file)
+    elif not sys.stdin.isatty():
+        input_ = sys.stdin
+
+    if not input_:
+        logger.error("can't find any input, please give a pdf file"
+        " or use -f to give a list of pdf file")
+        return 2
+
+    for line in input_:
+        if not line.startswith('#'):
+            indexer.index(line.strip())
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='''Search repo to index all book
     If there are queue files in root path of repo, it will only deal with those
     queue files. Otherwise it could walk through the whole repo to index them
     all.''')
-    parser.add_argument('db_path')
-    parser.add_argument('repo_path')
+    parser.add_argument('pdf', nargs='?', help='pdf path to index')
+    parser.add_argument('-d', '--db-path', default='db',
+        help='xapian db root path')
+    parser.add_argument('-r', '--repo-path', default='repo',
+        help='ebook repo root path')
+    parser.add_argument('-f', '--pdf-list-file', type=os.path.abspath,
+        help='given a file each line is a pdf path. '
+        'This option conflict with the positional argument "pdf"')
     parser.add_argument('-v', '--verbose',
         action='store_true', help='turn on verbose mode')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-    opts = parse_args()
+    args = parse_args()
 
-    level = logging.DEBUG if opts.verbose else logging.INFO
+    level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=level, stream=sys.stdout)
 
-    sys.exit(index(opts.db_path, opts.repo_path, opts))
+    sys.exit(main(args))
